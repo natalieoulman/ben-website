94ef80c1529e3d0623fe15d27427cf5bdafc211c
Natalie Oulman <natalie.oulman@gmail.com>
2021-04-05 20:09:59 +0000

    Added templates for bens fees contact info and reviews

--- server.py
+++ server.py
@@ -17,5 +17,26 @@ def homepage():
     return render_template('home.html')
 
 
+@app.route('/benviews')
+def ben_reviews():
+    """Returns a review page of Ben's work"""
+
+    return render_template('benviews.html')
+
+
+@app.route('/bens-contact')
+def ben_contact():
+    """Returns a contact page for Ben"""
+
+    return render_template('bens_contact.html')
+
+
+@app.route('/bens-fees')
+def ben_fee():
+    """Returns a list of fees for Ben's services"""
+
+    return render_template('bens_fees.html')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
